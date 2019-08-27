613a52658d161a9acb4a27ffe2bfd36504b25cff
Taras <petryshak.taras@gmail.com>
2019-08-27 15:35:03 +0300

    add files of resume

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+const changeSection = document.querySelector('.contact-info');
+body.addEventListener('click', (e) => {
+    if (e.target.classList.contains('contact-btn')) {
+        changeSection.style.transition = '1s';
+        changeSection.style.transform = 'scale(2)';
+        changeSection.style.backgroundColor = 'rgba(250, 143, 100, 0.925)';
+        changeSection.style.borderRadius = '1.5rem';
+    } else {
+        changeSection.style.transform = 'scale(1)';
+        changeSection.style.backgroundColor = 'inherit';
+        changeSection.style.borderRadius = 'none';
+    }
+})
