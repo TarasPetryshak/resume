ed98fefe334e33e77cb39872add78e633508e987
Taras <petryshak.taras@gmail.com>
2019-11-12 19:03:53 +0200

    add three portfolio links

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-/* $(document).ready(function() {
-    $('#slider-1').owlCarousel({
-        rtl: false,
-        nav: false,
-        dots: false,
-        loop: true,
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        autoplayTimeout: 3000,
-    })
-}); */
 $(document).ready(function() {
     $('.certificates').owlCarousel({
         rtl: false,
@@ -22,6 +10,8 @@ $(document).ready(function() {
         autoplayTimeout: 8000,
     })
 });
+
+let scrollButton = $('#scroll-button');
 let links = $('.about__block_link');
 let setPosition = () => {
     $('html,body').animate({
@@ -32,14 +22,14 @@ let animationContact = () => {
     let count = 0;
     let timer = setInterval(() => {
         if (count != 0) {
-            $(links[count-1]).toggleClass('animation-contact');
+            $(links[count - 1]).toggleClass('animation-contact');
         }
         $(links[count]).toggleClass('animation-contact');
         count++;
         if (count > links.length) {
             console.log(count);
             clearInterval(timer);
-            $(links[count-1]).toggleClass('animation-contact');
+            $(links[count - 1]).toggleClass('animation-contact');
 
         }
     }, 700);
@@ -49,3 +39,22 @@ $('#contacts').click(() => {
     setPosition();
     animationContact();
 })
+$(document).ready(scrollButton.fadeOut(10));
+let visibleScrollButton = () => {
+    if ($(window).scrollTop() >= 150) {
+        scrollButton.fadeIn(100);
+    } else {
+        scrollButton.fadeOut(100);
+    }
+}
+$(window).scroll(visibleScrollButton);
+scrollButton.click(() => {
+        $('html').animate({
+            scrollTop: 0
+        }, 300)
+    })
+    /*
+    $(window).load(function() {
+      $('.preloader').fadeOut(500);
+        $('.preloader').fadeOut(500);
+    }) */
